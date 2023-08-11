ec2855ceec209ba98cddc64d60cc971b783460e6
404-lorena <lramirezlopez@google.com>
2023-08-11 10:14:57 -0400

    Added Image Composable

--- app/src/main/java/com/dalelore/nasa/MainActivity.kt
+++ app/src/main/java/com/dalelore/nasa/MainActivity.kt
@@ -3,13 +3,18 @@ package com.dalelore.nasa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dalelore.nasa.ui.theme.NASATheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +27,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NasaImg()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun NasaImg(){
+    Column() {
+        Image(
+            painter = painterResource(R.drawable.nasa_foto),
+            contentDescription = "NASA foto of space",
+            modifier = Modifier.height(220.dp)
+        )
     }
 }
 
